dfd99a9132b30e7b41dbfd3f20b62b9595ebad5b
Friedrich Groestlinger <friedrich.groestlinger@plasmo.eu>
2015-01-30 16:03:20 +0100

    changed table widget to table view using table model

--- log_parser.py
+++ log_parser.py
@@ -51,81 +51,130 @@ class LogEntry(object):
         else:
             return None
             
+    def __str__(self):
+        return "{0}) {1}: {2} {3}".format(self.line_num, self.date_time, self.log_type, self.message)
+            
             
 class LogTableModel(QtCore.QAbstractTableModel):
     
     _column_count = 4
+    log_types_changed = QtCore.pyqtSignal()
     
     def __init__(self, file_name, parent=None, *args):        
-        super(LogTableModel, self).__init__(self, parent, *args)
-        self.file_name = file_name        
+        super(LogTableModel, self).__init__(parent, *args)
+        self._file_name = file_name        
         self._current_log_type = ""
         self._is_log_type_active = False
+        self._update_log_types = False
         self._regex_string = ""
-        self._load_data()
+        self._log_types = []       
+        self._log_entries = []
+        self.update()
+
+    def _get_file_name(self):
+        return self._file_name
+        
+    def _set_file_name(self, file_name):
+        self._file_name = file_name
+        self.update()
+        
+    file_name = property(_get_file_name, _set_file_name)
        
     def _get_log_type(self):
         return self._current_log_type
        
     def _set_log_type(self, log_type):
         self._current_log_type = log_type
-        self.update()
+        self.update(False)
         
     log_type = property(_get_log_type, _set_log_type)
+    
+    def _get_log_types(self):
+        return self._log_types
+        
+    log_types = property(_get_log_types)
     
     def _get_is_log_type_active(self):
         return self._is_log_type_active
         
     def _set_is_log_type_active(self, is_log_type_active):
         self._is_log_type_active = is_log_type_active
+        self.update(False)
+     
+    is_log_type_active = property(_get_is_log_type_active, _set_is_log_type_active)
+    
+    def _get_regex_string(self):
+        return self._regex_string
+        
+    def _set_regex_string(self, regex_string):
+        self._regex_string = str(regex_string)
         self.update()
+        
+    regex_string = property(_get_regex_string, _set_regex_string)
     
     def rowCount(self, parent):
-        return len(self.log_entries)
+        return len(self._log_entries)
     
     def columnCount(self, parent):
         return LogTableModel._column_count
     
     def data(self, index, role):
         result = QtCore.QVariant()
-        if index.isValid() and role == QtCore.Qt.DisplayRole:           
+        if index.isValid():
             row = index.row()
             col = index.column()
-            if col == 0:
-                result = str(self.log_entries[row].line_num)
-            elif col == 1:
-                result = self.log_entries[row].log_type
-            elif col == 2:
-                result = self.log_entries[row].date_time.__str__()
-            elif col == 3:
-                result = self.log_entries[row].message
+            if role == QtCore.Qt.DisplayRole:           
+                if col == 0:
+                    result = str(self._log_entries[row].line_num)
+                elif col == 1:
+                    result = self._log_entries[row].log_type
+                elif col == 2:
+                    result = self._log_entries[row].date_time.__str__()
+                elif col == 3:
+                    result = self._log_entries[row].message
+            elif role == QtCore.Qt.BackgroundColorRole:
+                log_type = self._log_entries[row].log_type
+                if log_type == "ERROR": result = QtGui.QBrush(QtGui.QColor(255, 0, 0, 200))
+                elif log_type == "WARN": result = QtGui.QBrush(QtGui.QColor(255, 125, 0, 130))
+                elif log_type == "INFO": result = QtGui.QBrush(QtGui.QColor(75, 255, 0, 80))
         return result
         
-    def _load_data(self):
-        log_entries = []
-        regex = re.compile(self.regex_string)           
-        log_types = []
-        with open(self.file_name, "r") as fid:
-            for i, line in enumerate(fid):
-                line_num = str(i + 1)
-                log_entry = LogEntry.from_log_line(line_num, line)
-                if log_entry:            
-                    if log_entry.log_type not in log_types:
-                        log_types.append(log_entry.log_type)
-                    if self._is_log_type_active \
-                        and self.current_log_type != log_entry.log_type and self.current_log_type != "":
-                        continue
-                    if regex == "":                      
-                        log_entries.append(log_entry)
-                    else:
-                        match = regex.search(log_entry.message)
-                        if (match):
-                            log_entries.append(log_entry)
+    def _load_data(self, update_log_types):
+        self._log_entries = []
+        regex = re.compile(self._regex_string)           
+        self._log_types = []
+        try:
+            with open(self.file_name, "r") as fid:
+                for i, line in enumerate(fid):
+                    line_num = str(i + 1)
+                    log_entry = LogEntry.from_log_line(line_num, line)
+                    if log_entry:            
+                        if log_entry.log_type not in self._log_types:
+                            self._log_types.append(log_entry.log_type)
+                        if self._is_log_type_active \
+                            and self._current_log_type != log_entry.log_type and self._current_log_type != "":
+                            continue
+                        if self._regex_string == "":
+                            self._log_entries.append(log_entry)
+                        else:
+                            match = regex.search(log_entry.message)
+                            if (match):
+                                self._log_entries.append(log_entry)
+            if update_log_types:
+                self.log_types_changed.emit()
+        except Exception as e:
+            print(e)
                             
-    def update(self):
-        self._load_data()
-        self.dataChanged()
-        
+    def update(self, update_log_types=True):
+        self._load_data(update_log_types)
+        idx_start = QtCore.QModelIndex()
+        idx_start.row = 0
+        idx_start.column = 0
+        idx_end = QtCore.QModelIndex()
+        idx_end.row = len(self._log_entries)
+        idx_end.column = LogTableModel._column_count
+#        self.dataChanged.emit(idx_start, idx_end)
+        self.layoutChanged.emit()     
         
 
 class LogParser(QtGui.QMainWindow):
@@ -139,7 +188,10 @@ class LogParser(QtGui.QMainWindow):
         uic.loadUi("log_parser.ui", self)
         self.actionOpen.triggered.connect(self._file_button_clicked)
         self.searchLineEdit.textChanged.connect(self._regex_text_changed)
-        self.logTable.verticalScrollBar().sliderReleased.connect(self._table_scrolled)
+        self._log_table_model = LogTableModel("")
+        self._log_table_model.log_types_changed.connect(self._log_types_changed)
+        self.logTableView.setModel(self._log_table_model)
+        self.logTableView.verticalScrollBar().sliderReleased.connect(self._table_scrolled)
         self.logTypeComboBox.currentIndexChanged.connect(self._combo_box_selection_changed)
         self.logTypeCheckBox.stateChanged.connect(self._check_box_changed)
         
@@ -147,78 +199,31 @@ class LogParser(QtGui.QMainWindow):
         start_dir = r"D:\Projekte\EOS\Online\EOSTATEMeltpoolTest\TestData\201501231400TestOneJobSuccess\Log"
         self.file_name = QtGui.QFileDialog.getOpenFileName(self, "Log file name", start_dir)
         self.fileLabel.setText(self.file_name)
-        self._update_log(self._get_regex_string())
-        
-    def _get_regex_string(self):
-        return str(self.searchLineEdit.text())
-        
-    @staticmethod
-    def _get_log_brush(log_type):
-        if log_type == "ERROR": background = QtGui.QBrush(QtGui.QColor(255, 0, 0, 200))
-        elif log_type == "WARN": background = QtGui.QBrush(QtGui.QColor(255, 125, 0, 130))
-        elif log_type == "INFO": background = QtGui.QBrush(QtGui.QColor(75, 255, 0, 80))
-        else: background = None
-        return background
+        self._log_table_model.file_name = self.file_name
+        self._format_log_table()     
     
-    @staticmethod
-    def _get_table_item(log_type, log_entry):
-        log_entry_item = QtGui.QTableWidgetItem(log_entry)
-        log_entry_brush = LogParser._get_log_brush(log_type)
-        if log_entry_brush:
-            log_entry_item.setBackground(log_entry_brush)
-        return log_entry_item      
+    def _log_types_changed(self):
+        self.logTypeComboBox.clear()
+        self.logTypeComboBox.insertItems(0, self._log_table_model.log_types)      
         
-    def _update_log(self, regex_string, update_combo_box=True):
-#        try:
-            self.logTable.clearContents()
-            self.logTable.setColumnCount(4)
-            log_entries = []
-            regex = re.compile(regex_string)
-            current_log_type = self.logTypeComboBox.currentText()           
-            log_types = []
-            with open(self.file_name, "r") as fid:
-                for i, line in enumerate(fid):
-                    line_num = str(i + 1)
-                    log_entry = LogEntry.from_log_line(line_num, line)
-                    if log_entry:            
-                        if log_entry.log_type not in log_types:
-                            log_types.append(log_entry.log_type)
-                        if self.logTypeCheckBox.isChecked() \
-                            and current_log_type != log_entry.log_type and current_log_type != "":
-                            continue
-                        if regex == "":                      
-                            log_entries.append(log_entry)
-                        else:
-                            match = regex.search(log_entry.message)
-                            if (match):
-                                log_entries.append(log_entry)
-            self.logTable.setRowCount(len(log_entries))
-            for i, log_entry in enumerate(log_entries):
-                self.logTable.setItem(i, 0, LogParser._get_table_item(log_entry.log_type, str(log_entry.line_num)))
-                self.logTable.setItem(i, 1, LogParser._get_table_item(log_entry.log_type, log_entry.log_type))
-                self.logTable.setItem(i, 2, LogParser._get_table_item(log_entry.log_type, log_entry.date_time.__str__()))
-                self.logTable.setItem(i, 3, LogParser._get_table_item(log_entry.log_type, log_entry.message))       
-#        except:
-#            pass
-            self.logTable.resizeColumnsToContents()
-            self.logTable.horizontalHeader().setStretchLastSection(True)
-            if update_combo_box:
-                self.logTypeComboBox.clear()
-                self.logTypeComboBox.insertItems(0, log_types)
+    def _format_log_table(self):
+        self.logTableView.resizeColumnsToContents()
+        self.logTableView.horizontalHeader().setStretchLastSection(True)
              
     def _regex_text_changed(self):
-        self._update_log(self._get_regex_string())
+        self._log_table_model.regex_string = str(self.searchLineEdit.text())
+        self._format_log_table()
         
     def _table_scrolled(self):
-        self.logTable.resizeColumnsToContents()
-        self.logTable.horizontalHeader().setStretchLastSection(True)
+        self._format_log_table()
         
     def _combo_box_selection_changed(self):
-        if self.logTypeCheckBox.isChecked():
-            self._update_log(self._get_regex_string(), False)
+        self._log_table_model.log_type = self.logTypeComboBox.currentText()
+        self._format_log_table()
             
     def _check_box_changed(self):
-        self._update_log(self._get_regex_string(), False)
+        self._log_table_model.is_log_type_active = self.logTypeCheckBox.isChecked()
+        self._format_log_table()
         
         
         
